6fcd3881950862490c17ae0d57b2a0feac33cda9
Timelas <lom.a.s@icloud.com>
2022-06-01 17:09:27 +0300

    search bag

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,7 +7,7 @@ const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    next(new NotAuth('Ошибка авторизации'));
+    next(new NotAuth('Ошибка авторизации1'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +16,7 @@ const auth = (req, res, next) => {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new NotAuth('Ошибка авторизации'));
+    next(new NotAuth('Ошибка авторизации2'));
   }
   req.user = payload;
   next();
